f596fc9e7939add7d1b3d1be7c68ad3045afeeb3
imAshutoshGupta <136306714+imAshutoshGupta@users.noreply.github.com>
2023-07-11 15:27:10 +0530

    Add files via upload

--- vowel-consonant.py
+++ vowel-consonant.py
@@ -4,3 +4,12 @@ if ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u":
     print("its a vowel")
 else:
     print("its a consonant")
+    
+    #OR
+    
+chac = input("enter the alphabet: ")
+
+if chac in ('a','e','i','o','u'):
+    print("its a vowel")
+else:
+    print("its a consonant")
