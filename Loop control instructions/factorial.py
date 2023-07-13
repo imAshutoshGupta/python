207d953495fc95e984de282e3799e95305158951
imAshutoshGupta <136306714+imAshutoshGupta@users.noreply.github.com>
2023-07-13 14:37:25 +0530

    Add files via upload

--- Loop control instructions/factorial.py
+++ Loop control instructions/factorial.py
@@ -8,3 +8,16 @@ while i<=n:
     s=s*i
     i=i+1
 print("Hence factorial of",n,"is",s)
+
+
+# DRY RUN
+#n=4
+#i      i<=4        s=s*i       i=i+1
+
+#i      i<=4        s=1         i=i+1=1+1
+#i      i<=4        s=1*2       i=i+1=2+1
+#i      i<=4        s=2*3       i=i+1=3+1
+#i      i<=4        s=6*4       i=i+1=4+1
+#i      i<=4        
+
+print("   ")
