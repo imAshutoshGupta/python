16dcd79d931dd93a44b9de501b3dc94bd8f21ad1
imAshutoshGupta <136306714+imAshutoshGupta@users.noreply.github.com>
2023-07-18 13:55:33 +0530

    Add files via upload

---
+++ Loop control instructions/for-factorial.py
@@ -0,0 +1,8 @@
+n = int(input("enter the number: "))
+i=1
+s=1
+
+for i in range(1,n+1,1):
+    s=s*i
+print(s)
+
---
+++ Loop control instructions/for-table.py
@@ -0,0 +1,6 @@
+n = int(input("enter your number: "))
+
+i=1
+for i in range(1,11,1):
+    r=n*i
+    print(r)
