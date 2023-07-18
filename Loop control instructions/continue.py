7a52b53db9398468ca33b72a9663d904fb6b13f7
imAshutoshGupta <136306714+imAshutoshGupta@users.noreply.github.com>
2023-07-18 14:41:01 +0530

    Add files via upload

---
+++ Loop control instructions/continue.py
@@ -0,0 +1,17 @@
+# CONTINUE IS ALWAYS USED WITH IF KEYWORD
+i=1
+while i<=10:
+    if i==4:
+        i=i+1
+        continue
+    print(i)
+    i=i+1
+
+
+i=1
+while i<=10:
+    if i==4:
+        print("hahaha")
+        continue
+    print(i)
+    i=i+1
