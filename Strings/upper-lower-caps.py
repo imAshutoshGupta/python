f26715486f3f724e30ad5c0f0e9737b332a7cd6f
Ashutosh Gupta <aguptaworkspace@gmail.com>
2023-08-01 14:08:28 +0530

    Add files via upload

---
+++ Strings/upper-lower-caps.py
@@ -0,0 +1,19 @@
+#upper | lower | caps
+
+s="itvedant"
+print(s)
+
+supper = s.upper()
+print(supper)
+
+slower = supper.lower()
+print(slower)
+
+scap = s.capitalize()
+print(scap)
+
+n = "9723974658"
+# n= "motorola1234"
+#isdigit returns true if the string is digit and vice versa
+res=n.isdigit()
+print(res)
