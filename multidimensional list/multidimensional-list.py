53c7508ccde1f0bb8932e8a9603851c3c9ecf26f
Ashutosh Gupta <aguptaworkspace@gmail.com>
2023-08-07 15:26:41 +0530

    Add files via upload

--- multidimensional list/multidimensional-list.py
+++ multidimensional list/multidimensional-list.py
@@ -4,3 +4,61 @@ print("entire nested list: ", l)
 print("list first element: ", l[0])
 print("list second element: ", l[1])
 print("list third element: ", l[2])
+
+
+# accessing each value (like a matrix)
+
+print("First row elements: ")
+print(l[0][0])
+print(l[0][1])      #i=0 j=0,1,2
+print(l[0][2])
+
+print("Second row elements: ")
+print(l[1][0])
+print(l[1][1])      #i=1 j=0,1,2
+print(l[1][2])
+
+print("Third row elements: ")
+print(l[2][0])
+print(l[2][1])      #i=2 j=0,1,2
+print(l[2][2])
+
+
+# accessing using loops 
+
+            
+# #dry run
+# i     j
+
+# 0     0
+#       1
+#       2
+
+# 1     0
+#       1
+#       2
+
+# 2     0
+#       1
+#       2
+
+for i in range(0,3,1):
+    for j in range(0,3,1):
+        print(l[i][j])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
