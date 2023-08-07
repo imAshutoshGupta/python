3b45269e5d323dac7b7f1f98fae87f22fc3bd4e9
Ashutosh Gupta <aguptaworkspace@gmail.com>
2023-08-07 15:02:44 +0530

    Add files via upload

---
+++ multidimensional list/multidimensional-list.py
@@ -0,0 +1,6 @@
+#multidimensional list
+l = [[11,9,7],[1,-2,5],[9,5,6]]
+print("entire nested list: ", l)
+print("list first element: ", l[0])
+print("list second element: ", l[1])
+print("list third element: ", l[2])
