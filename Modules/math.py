f4b821b49e284a9662fa9183936b23b72510acdd
Ashutosh Gupta <aguptaworkspace@gmail.com>
2023-08-14 14:27:06 +0530

    Add files via upload

---
+++ Modules/math.py
@@ -0,0 +1,7 @@
+# math module
+# import modulename
+
+import math
+x = math.pi
+print("value of pi:", x)
+print("value of tau:", math.tau)
---
+++ Modules/modules.py
@@ -0,0 +1,13 @@
+''' # MODULE
+
+Need of modules - To reuse code across various python files
+
+What is a module? - Module is a python file which is a collection of functions, constants and classes
+
+Types of modules
+1) Built-In modules
+2) User-defined modules
+
+
+
+'''
