4b7508c17f3960de7f6f38046944e06deca89d1c
Ashutosh Gupta <aguptaworkspace@gmail.com>
2023-08-16 15:17:03 +0530

    Add files via upload

---
+++ File Handling System/file-handling.py
@@ -0,0 +1,26 @@
+'''
+File handling system
+
+Need of FH - To store data permanently there is a need of file
+
+What is FHS - in order to perform manipulation with data stored in the file we use FHS
+
+Writing into a file:
+
+Database <---------> .py file <----------> console or output
+            write         |         i/p
+                          |
+                          |
+                variable temporary storage
+
+Reading from file:
+
+Database <-------------> .py file <----------> console or output
+            read()         |         print()
+          readlines()      |
+                           |
+                           |
+                          data
+
+
+'''
