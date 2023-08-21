0f9e8c09b195589d104b9b63b88efc8d4574f9dd
Ashutosh Gupta <aguptaworkspace@gmail.com>
2023-08-21 14:58:45 +0530

    Add files via upload

--- Exception Handling/division-of-two-numbers.py
+++ Exception Handling/division-of-two-numbers.py
@@ -2,7 +2,7 @@
 # error - when you put input as zero
 
 n=int(input("Enter numerator: "))
-d=int(input("enter denominator: "))
+d=int(input("enter denominator:     "))
 div=n/d
 print ("Division is: " , div)
 print(type(div))
@@ -20,3 +20,28 @@ except ZeroDivisionError:
     print("Denominator cannot be zero")
 except ValueError:
     print("Please enter the value in digits only")
+
+# cant type exceptions one by one, hence we use a universal exception (you cant give personalized messages tho)
+
+try:    
+    n=int(input("Enter numerator: "))
+    d=int(input("enter denominator: "))
+    div=n/d
+    print ("Division is: " , div)        
+    print(type(div))        
+
+except Exception:
+    print("Something went wrong!!!")
+
+    # only technical definition of error
+
+try:    
+    n=int(input("Enter numerator: "))
+    d=int(input("enter denominator: "))
+    div=n/d
+    print ("Division is: " , div)       
+    print(type(div))        
+
+except Exception as e:      #object
+    print("Error: ",e)
+
